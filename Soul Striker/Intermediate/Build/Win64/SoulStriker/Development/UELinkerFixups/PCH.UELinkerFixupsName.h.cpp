87282fb7e09c6d9b587582f458d774098f21ed19
Alex Compton <lxcompton@gmail.com>
2017-05-24 14:55:07 -0400

    Don't Fail Me Now
    Oh BOIIIIIIIIIIII

---
+++ Soul Striker/Intermediate/Build/Win64/SoulStriker/Development/UELinkerFixups/PCH.UELinkerFixupsName.h.cpp
@@ -0,0 +1,1 @@
+#include "C:\Users\Steve\Documents\SSRepository\Soul-Striker-Main\Soul Striker\Intermediate\Build\Win64\UE4\Development\UELinkerFixupsName.h"
---
+++ Soul Striker/Intermediate/Build/Win64/SoulStriker/Development/UELinkerFixups/UELinkerFixupsName.h
@@ -0,0 +1,5 @@
+#ifndef __AUTO_UELINKERFIXUPSNAME_H__
+#define __AUTO_UELINKERFIXUPSNAME_H__
+//Last Write: 5/7/2017 5:32:57 PM +00:00
+#include "C:\Users\Steve\Documents\SSRepository\Soul-Striker-Main\Soul Striker\Intermediate\Build\Win64\UE4\Development\UELinkerFixupsName.h"
+#endif//__AUTO_UELINKERFIXUPSNAME_H__
